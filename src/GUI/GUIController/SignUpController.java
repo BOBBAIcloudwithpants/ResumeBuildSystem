dafe54bfe9d3b72757f60e5b70cc2806b5cfea54
UUUtoma <842567642@qq.com>
2019-12-10 19:52:56 +0800

    更新登录页面，增加教师详情页面初始。

--- src/GUI/GUIController/SignUpController.java
+++ src/GUI/GUIController/SignUpController.java
@@ -1,44 +1,86 @@
 package GUI.GUIController;
 
 import Controller.UserController;
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.input.InputMethodEvent;
 import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.PasswordField;
 
 import Controller.UserController.*;
 
 public class SignUpController {
 
     @FXML
-    private TextField user_name_box;
+    private TextField username;
 
     @FXML
-    private Button sign_up_button;
+    private PasswordField password;
 
     @FXML
-    private ToggleGroup user_identity;
+    private PasswordField passwordConfirm;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private RadioButton isTeacher;
+
+    @FXML
+    private ToggleGroup userAdmin;
+
+    @FXML
+    private RadioButton isStudent;
 
     @FXML
     private Text errmsg;
+
 
     @FXML
     void signUp(ActionEvent event) {
         UserController user_controller = new UserController();
         //check username
-        if (user_name_box.getText().equals("")) {
+        if (username.getText().equals("")) {
             errmsg.setText("用户名不能为空");
             return;
         }
-        boolean valid_name = user_controller.findUserByName(user_name_box.getText());
-        if (!valid_name) {
-            errmsg.setText("用户名已被使用");
+
+        //check password
+        if (password.getText().length() == 0) {
+            errmsg.setText("密码不能为空");
             return;
         }
-        //check password
-        errmsg.setText("submit press");
+        if (!password.getText().equals(passwordConfirm.getText())) {
+            errmsg.setText("两次密码输入不一致");
+            return;
+        }
+
+        //check admin
+        if (userAdmin.getSelectedToggle() == null) {
+            errmsg.setText("请选择身份");
+            return;
+        }
+
+        int isAdmin = (isTeacher.isSelected()) ? 1 : 0;
+
+        UserController user = new UserController();
+        boolean success = user.registerUser(username.getText(), password.getText(), isAdmin);
+        if (!success) {
+            errmsg.setText("用户已存在");
+        }
+        else {
+            if(isAdmin == 1){
+                MainApp.gotoTeacherPage(username.getText());
+            }
+            else{
+                MainApp.gotoStudentPage(username.getText());
+            }
+        }
     }
+
 
 }
--- src/GUI/GUIController/TeacherPageController.java
+++ src/GUI/GUIController/TeacherPageController.java
@@ -1,4 +1,113 @@
 package GUI.GUIController;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+
+import Controller.UserController;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.User;
+
+import java.util.List;
+import java.util.ListIterator;
+
 public class TeacherPageController {
+    @FXML
+    private Text username;
+
+    @FXML
+    private Text groupID;
+
+    @FXML
+    private TableView<ScoreInforForTeacher> studentScoreTable;
+
+    @FXML
+    private TableColumn<ScoreInforForTeacher, String> studentNameColumn;
+
+    @FXML
+    private TableColumn<ScoreInforForTeacher, Integer> scoreColumn;
+
+    @FXML
+    private MenuButton subjectSelector;
+
+    @FXML
+    private MenuItem subject1;
+
+    @FXML
+    private MenuItem subject2;
+
+    @FXML
+    private MenuItem subject3;
+
+    private final ObservableList<ScoreInforForTeacher> data = FXCollections.observableArrayList();
+
+    //根据科目更新tableView
+    public void reset(String teacherName, int subjectID) {
+        UserController controller = new UserController();
+        //get all students
+        User teacher = controller.getUserByUsername(teacherName);
+        int groupID = teacher.getGroupID();
+        List<User> students =  controller.getStudentsByGroupID(groupID);
+        //get grade by subject
+        //default subject1
+        for (int i =0; i< students.size();i++) {
+            User student = students.get(i);
+            String studentName = student.getUsername();
+            int grade = controller.getGradeByUsernameAndSubject(studentName, subjectID);
+            data.add(new ScoreInforForTeacher("subject" + subjectID , studentName, grade));
+        }
+
+        studentNameColumn.setCellValueFactory(data->data.getValue().getStudentName());
+        scoreColumn.setCellValueFactory(data->data.getValue().getScore().asObject());
+    }
+
+    //处理科目选择的逻辑
+
+    //处理饼图
+
+
+}
+
+class ScoreInforForTeacher {
+    private final StringProperty t_subject;
+    private final StringProperty t_studentName;
+    private final IntegerProperty t_score;
+
+    public ScoreInforForTeacher(String subject, String studentName, Integer score){
+        t_subject = new SimpleStringProperty(subject);
+        t_studentName = new SimpleStringProperty(studentName);
+        t_score = new SimpleIntegerProperty(score);
+    }
+
+    public void setSubject(String subject){
+        t_subject.set(subject);
+    }
+
+    public StringProperty getSubject(){
+        return t_subject;
+    }
+
+    public void setScore(int score){
+        t_score.set(score);
+    }
+
+    public IntegerProperty getScore(){
+        return t_score;
+    }
+
+    public void setStudentName(String studentName){
+        t_studentName.set(studentName);
+    }
+
+    public StringProperty getStudentName(){
+        return t_studentName;
+    }
 }
